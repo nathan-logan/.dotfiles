03a11e542e99a1bcdf576f6eb044d41eadbd4a85
Nathan Logan <nathanlogan22@gmail.com>
2025-05-17 21:20:47 +1000

    nvim: disable right click context menu

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,11 @@ require("lsp")
 require("keymaps")
 
 vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#363433" })
+
+-- Disable NVIM right click context menu
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		vim.api.nvim_command([[aunmenu PopUp]])
+		vim.api.nvim_command([[autocmd! nvim.popupmenu]])
+	end
+})
