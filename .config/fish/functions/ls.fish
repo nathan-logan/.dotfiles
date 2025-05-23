b3c4f5e64bad09c1a9b64fe91f62a5a3558dd859
Nathan Logan <nathanlogan22@gmail.com>
2025-05-24 07:18:41 +1000

    config: move n^3 folder into .config

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
-function ls --wraps='nnn -de' --description 'alias ls=nnn -de'
-  nnn -de $argv
+function ls --wraps='nnn -de' --wraps='nnn -deH' --description 'alias ls=nnn -deH'
+  nnn -deH $argv
         
 end
