b7d5b3e634d4d0870ea07749581017d138dcebb2
Nathan Logan <nathanlogan22@gmail.com>
2025-06-12 11:56:36 +1000

    fix: fix text-case plugin, update all plugins

--- .config/nvim/lua/plugins/text-case.lua
+++ .config/nvim/lua/plugins/text-case.lua
@@ -1,4 +1,9 @@
 -- Replace text by case, i.e camelCase to snake_case
 return {
   "johmsalas/text-case.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  config = function()
+    require("textcase").setup({})
+    require("telescope").load_extension("textcase")
+  end,
 }
