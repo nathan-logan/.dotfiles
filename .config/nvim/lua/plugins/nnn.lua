7b4ec6de959afe24b1b8a40a0ca7529d1da35a39
Nathan Logan <nathanlogan22@gmail.com>
2026-01-20 09:49:24 +1000

    feat: add -A flag to nnn so directories don't auto open during filtering

--- .config/nvim/lua/plugins/nnn.lua
+++ .config/nvim/lua/plugins/nnn.lua
@@ -3,7 +3,10 @@ return {
     config = function()
         require("nnn").setup({
             picker = {
-                cmd = "tmux new-session nnn -Pp -H",
+                cmd = "tmux new-session nnn -Pp -H -A",
+                -- -Pp uses preview-tui plugin
+                -- -H shows hidden files by default
+                -- -A no dir auto enter during filter
                 style = { width = 1, height = 0.95, xoffset = 0, yoffset = 0, border = "rounded" },
                 session = "shared",
                 offset = true
